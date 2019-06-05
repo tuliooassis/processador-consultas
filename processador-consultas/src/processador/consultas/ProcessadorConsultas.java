f8d7a2e1a620a6038ddbbb646173dfd7fce5e68f
felipesafreitas <felipesafreitas@gmail.com>
2019-06-05 08:31:16 -0300

    reading files

--- processador-consultas/src/processador/consultas/ProcessadorConsultas.java
+++ processador-consultas/src/processador/consultas/ProcessadorConsultas.java
@@ -5,6 +5,14 @@
  */
 package processador.consultas;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author Aluno
@@ -16,6 +24,44 @@ public class ProcessadorConsultas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        StringBuilder contentBuilder = new StringBuilder();
+        
+        try {
+            for (File file : listFileTree(new File("../idx/"))) {
+                BufferedReader in = new BufferedReader(new FileReader(file));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    contentBuilder.append(str);
+                }
+                in.close();
+                
+                String content = contentBuilder.toString();
+                contentBuilder = new StringBuilder();
+                
+                // criar indice
+                // fazer consulta
+            }
+        }
+        
+        catch (IOException e) {
+            System.err.print(e);
+        }
     }
     
+    public static Collection<File> listFileTree(File dir) {
+        Set<File> fileTree = new HashSet<File>();
+        if (dir == null || dir.listFiles() == null) {
+            return fileTree;
+        }
+        for (File entry : dir.listFiles()) {
+            if (entry.isFile()) {
+                fileTree.add(entry);
+            } else {
+                fileTree.addAll(listFileTree(entry));
+            }
+        }
+        return fileTree;
+    }
+    
+
 }
